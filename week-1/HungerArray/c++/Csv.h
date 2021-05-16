ef9290cf6ee8b6b916513a373ae4bc037805b471
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2021-05-16 12:37:46 +0545

    c++: Added csv class file

---
+++ week-1/HungerArray/c++/Csv.h
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <string>
+
+namespace Sol
+{
+    class Csv
+    {
+    public:
+    
+        Csv(const std::string &);
+
+        Csv(const Csv &) = delete;
+        Csv &operator=(const Csv &) = delete;
+
+        Csv(Csv &&);
+        Csv &operator=(Csv &&);
+
+        Csv &operator>>(std::string &);
+
+        bool eof();
+
+    private:
+        std::ifstream file_handle;
+    };
+}
