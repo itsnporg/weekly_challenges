2e6aca8c62ef657c42c3ef21b958ded02df43258
Ziaul Hasan Hamim <ziaulhasanhamim@outlook.com>
2021-05-21 23:58:08 +0600

    Add Solution of ziaulhasanhamim

---
+++ week-1/ZiaulHasanHamim/JsonWriter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZiaulHasanHamim
+{
+    public class JsonWriter
+    {
+        public static async Task WriteToJsonFile(List<Dictionary<string, string>> data, string fileName = "result.json")
+        {
+            StringBuilder jsonData = new();
+            jsonData.Append("[");
+            int i = 0;
+            foreach (var row in data)
+            {
+                jsonData.Append(addNewJsonObject(row));
+                i++;
+                if (i != data.Count)
+                {
+                    jsonData.Append(", ");
+                }
+            }
+            jsonData.Append("]");
+            await File.WriteAllTextAsync(fileName, jsonData.ToString());
+        }
+
+        private static void addNewJsonField(StringBuilder jsonObject, KeyValuePair<string, string> keyValue)
+        {
+            jsonObject.Append($"\"{keyValue.Key}\": ");
+            if (double.TryParse(keyValue.Value, out var numberValue))
+            {
+                jsonObject.Append(numberValue);
+            }
+            else
+            {
+                jsonObject.Append($"\"{keyValue.Value}\"");
+            }
+        }
+
+        private static string addNewJsonObject(Dictionary<string, string> row)
+        {
+            StringBuilder jsonObject = new();
+            jsonObject.Append("{");
+            int j = 0;
+            foreach (var keyValue in row)
+            {
+                addNewJsonField(jsonObject, keyValue);
+                j++;
+                if (j != row.Count)
+                {
+                    jsonObject.Append(", ");
+                }
+            }
+            jsonObject.Append("}");
+            return jsonObject.ToString();
+        }
+    }
+}
---
+++ week-1/ZiaulHasanHamim/README.md
@@ -0,0 +1,7 @@
+﻿# WEEK 1:  This is my solution to csv to json. 
+In my solution i used c# with dotnet 5. c# has got some really great libraries for working with csv and json (in the standard lib). But Here i didn't use any standard or third party lib. This a solution from scartch.
+
+> Requirement: .net 5
+- https://dotnet.microsoft.com/download/dotnet/5.0
+RUN: `dotnet run -c release` or you can first build and then run the exe
+OUTPUT: result.json will be created!!
